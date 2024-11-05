eb6092e06bf4a7781bc4d3c78c212d5d508a4d7d
Kirill Marchenko <mar4elos_91@mail.ru>
2024-11-05 19:20:51 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,22 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -10,5 +26,16 @@ public class Cat {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
         String say = peppy.sound();
+
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("cutlet");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
     }
 }
