5d223632eff2a3f72137eecdd45f9a9d7fa917ae
Nikita Marchenko <nrmarchenko@gmail.com>
2025-01-16 21:25:02 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,9 +5,27 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item first = new Item();
+       /* Item first = new Item();
         LocalDateTime time = first.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println("Текущая дата: " + time.format(formatter));
+        System.out.println("Текущая дата: " + time.format(formatter));*/
+        int a = 8;
+        int b = 3;
+        int result = 0;
+        int length = 1;
+        int section = 1;
+        int sum = 0;
+        int num = 1234;
+       /* for (int i = a; i <= b; i = i + 2) {
+            if (i % 2 == 1) {
+                result++;
+            }
+        }*/
+
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
     }
 }
