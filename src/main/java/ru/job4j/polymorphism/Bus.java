022eafecfa4ceb405a9550baede9bcfed016d5a8
Nikita Marchenko <nrmarchenko@gmail.com>
2025-03-05 20:20:25 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,28 @@
+package ru.job4j.polymorphism;
+
+import java.sql.SQLOutput;
+
+public class Bus implements  Transport {
+    @Override
+    public void move() {
+        System.out.println("Двигаюсь по маршруту к следующей остановке");
+    }
+
+    @Override
+    public void passengers(int count) {
+        System.out.println("Автобус вмещает в себя " + count + " пассажиров");
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        System.out.println("Заправка обошлась в " + fuel * 45 + " рублей");
+        return (fuel * 45);
+    }
+
+    public static void main(String[] args) {
+        Bus test = new Bus();
+        test.move();
+        test.passengers(10);
+        test.refuel(40);
+    }
+}
