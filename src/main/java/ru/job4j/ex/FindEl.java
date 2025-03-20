45744d3838d8238cddd51495b5f6767811b1f4e6
Nikita Marchenko <nrmarchenko@gmail.com>
2025-03-20 23:15:55 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = new String[] {"1", "2", "3", "4"};
+        String key = "5";
+        try {
+            indexOf(value, key);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
