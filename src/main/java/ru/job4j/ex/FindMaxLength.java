52c8bfb2ba1ebe7a1dc784bfa9409dd27c1dff96
Nikita Marchenko <nrmarchenko@gmail.com>
2025-03-22 13:59:10 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/FindMaxLength.java
+++ src/main/java/ru/job4j/ex/FindMaxLength.java
@@ -4,12 +4,9 @@ public class FindMaxLength {
     public static void main(String[] args) {
         String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
         int max = 0;
-        for (int index = 0; index < shops.length; index++) {
-            String element = shops[index];
-            if (element != null) {
-                if (element.length() > max) {
-                    max = element.length();
-                }
+        for (String element : shops) {
+            if (element != null && element.length() > max) {
+                max = element.length();
             }
         }
         System.out.println("Max length : " + max);
