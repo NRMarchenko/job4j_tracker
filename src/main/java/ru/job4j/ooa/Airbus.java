685e40a3084a4019024445479bf0436f6b6d6233
Nikita Marchenko <nrmarchenko@gmail.com>
2025-04-20 22:22:42 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -24,7 +24,8 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println("Количество двигателей равно: " + getName() == "A380" ? COUNT_ENGINE_A380 : COUNT_ENGINE);
+        System.out.println("Количество двигателей равно:");
+        System.out.println(name.equals("A380") ? COUNT_ENGINE_A380 : COUNT_ENGINE);
     }
 
     @Override
