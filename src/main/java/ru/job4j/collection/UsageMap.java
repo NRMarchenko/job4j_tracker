5176eb8966e4000a51113e1051730050c70e01d4
Nikita Marchenko <nrmarchenko@gmail.com>
2025-06-19 23:48:25 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("nrmarchenko@gmail.com", "Marchenko Nikita");
+        map.put("vvputin@gmail.com", "Putin Vladimir");
+        map.put("krmarchenko@gmail.com", "Marchenko Kirill");
+        map.put("nrmarchenko@gmail.com", "Marchenko Nikita");
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
         }
